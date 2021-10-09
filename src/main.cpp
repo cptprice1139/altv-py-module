83ae25c4ef86ecca379f1df5696fbd6f65310888
elsucco <64929401+elsucco@users.noreply.github.com>
2021-10-09 19:33:17 +0200

    add cmake  / edit gitignore

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marlon on 09/10/2021.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Marlon on 09/10/2021.
+//
+
+#ifndef ALTV_PY_MODULE_MAIN_H
+#define ALTV_PY_MODULE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ALTV_PY_MODULE_MAIN_H
